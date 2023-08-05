f72af28ee9aadc5c7e940536357a60aaa0a21f9a
8086021001 <anoopa413@gmail.com>
2023-08-05 12:17:48 +0530

    try

--- client/vite-project/src/pages/Landing.jsx
+++ client/vite-project/src/pages/Landing.jsx
@@ -1,8 +1,15 @@
+import { Button } from '@mui/material'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Landing = () => {
+    const navigate = useNavigate()
     return (
-        <div>Landing</div>
+        <div>
+            <Button onClick={navigate('/login')}>
+                click
+            </Button>
+        </div>
     )
 }
 
