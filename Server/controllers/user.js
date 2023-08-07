75bd374658b8723ba443a0cef8fbf07e63a583d3
8086021001 <anoopa413@gmail.com>
2023-08-07 11:08:02 +0530

    test push

--- Server/controllers/user.js
+++ Server/controllers/user.js
@@ -213,7 +213,7 @@ const login = async(req,res)=>{
                         httpOnly: true,
                         sameSite: 'none',
                         secure: true,
-                        domain:'curious-one-in.netlify.app'
+                        domain:".netlify.app"
                     }).json({message: "Logged in!", user: user, token})
                 } else {
                     return res.status(404).json({message: "Invalid password!"})
